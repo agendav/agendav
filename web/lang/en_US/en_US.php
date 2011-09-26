d5007be9e6b502cbc2a5f209e0c2066eea8e0949
Jorge López Pérez <jorge@adobo.org>
2011-09-26 14:50:41 +0200

    Start translation file for English

---
+++ web/lang/en_US/en_US.php
@@ -0,0 +1,26 @@
+<?php
+
+$labels = array();
+$messages = array();
+
+// Labels
+
+$labels['username'] = 'User name';
+$labels['passwd'] = 'Password';
+
+// Messages
+$messages['bad_login'] = 'Invalid username or password';
+$messages['element_not_found'] = 'Element not found';
+$messages['element_modified'] = 'Element was modified while you were editing
+it. Please, refresh.';
+$messages['unknown_http_code'] = 'Unknown error, HTTP code=$res';
+$messages['no_internal_calendar_name'] = 'Empty internal calendar name'];
+$messages['no_calendar_name'] = 'Empty calendar name';
+$messages['no_calendar_color'] = 'Color must be supplied';
+$messages['mkcalendar_invalid_params'] = 'Server refused to create calendar. Please, check your creation parameters';
+$message['server_forbidden'] = 'Server refused your request (permission forbidden)';
+$messages['setacl_invalid_xml'] = 'Internal error when generating permissions';
+$messages['setacl_invalid_users'] = 'Some of the users you specified do not exist'
+$messages['invalid_dialog_call'] = 'Interface error: invalid call to $func';
+$messages['not_implemented'] = '$feature: still not implemented';
+
