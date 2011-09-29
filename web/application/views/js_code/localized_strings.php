f70df4881e6cc4bc129a1585a45355e1e8b2937d
Jorge López Pérez <jorge@adobo.org>
2011-09-29 14:52:50 +0200

    Localize some JS strings, not complete

---
+++ web/application/views/js_code/localized_strings.php
@@ -0,0 +1,27 @@
+<?php
+$labels = $this->i18n->dump('labels');
+$js_messages = $this->i18n->dump('js_messages');
+
+$i18n = array('labels' => $labels, 'js_messages' => $js_messages);
+
+?>
+//<![CDATA[
+function _(mtype, s) {
+	var ret = '[' + mtype + ':' + s + ']';
+
+	if (typeof(i18n)!= 'undefined' && (mtype == 'js_messages' 
+			|| mtype == 'labels')) {
+		if (mtype == 'labels' && i18n.labels[s]) {
+			ret = i18n.labels[s];
+		} else if (mtype == 'js_messages' && i18n.js_messages[s]) {
+			ret = i18n.js_messages[s];
+		}
+	}
+
+	return ret;
+}
+
+var i18n = JSON.parse(<?php echo var_export(json_encode($i18n)) ?>);
+//]]>
+<?php
+// vim: set ft=javascript
