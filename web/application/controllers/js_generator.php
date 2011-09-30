b27687b5142eb245adf6b3fe205432eb0489f4f7
Jorge López Pérez <jorge@adobo.org>
2011-09-30 19:48:40 +0200

    Fix anonymous access problem to i18n js

--- web/application/controllers/js_generator.php
+++ web/application/controllers/js_generator.php
@@ -21,12 +21,18 @@
 
 class Js_generator extends CI_Controller {
 
+	// Special methods that do should not enforce authentication
+	private $not_enforced = array(
+			'i18n',
+			);
+
 	function __construct() {
 		parent::__construct();
 
 		define('SPECIAL_REQUEST', TRUE);
 
-		if (!$this->auth->is_authenticated()) {
+		if (!in_array($this->uri->segment(2), $this->not_enforced) &&
+				!$this->auth->is_authenticated()) {
 			$this->extended_logs->message('INTERNALS', 
 					'Anonymous access attempt to '
 					. uri_string());
