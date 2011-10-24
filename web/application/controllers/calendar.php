c653534252a67fbe8c1f1bd3307dcbbbd00f323b
Jorge López Pérez <jorge@adobo.org>
2011-10-24 19:15:01 +0200

    Minimize qTip size

--- web/application/controllers/calendar.php
+++ web/application/controllers/calendar.php
@@ -41,7 +41,7 @@ class Calendar extends CI_Controller {
 					'loadmask-0.4/jquery.loadmask.min.js',
 					'jquery.colorPicker.20091014.min.js',
 					'tag-it.20110621.min.js',
-					'jquery.qtip.20111022.js',
+					'jquery.qtip.20111022.min.js',
 					),
 				'css' => array(
 					array(
