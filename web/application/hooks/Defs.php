a2b7f53b7bb3aeef5e6fc7acb2ce872fcfa134d9
Jorge López Pérez <jorge@adobo.org>
2011-11-10 14:10:08 +0100

    Change JS order

--- web/application/hooks/Defs.php
+++ web/application/hooks/Defs.php
@@ -25,12 +25,12 @@ class Defs {
 			'libs/jquery-ui-1.8.16.min.js',
 			'libs/jquery.freeow.min.js',
 			'libs/fullcalendar.min.js',
-			'libs/jquery.timePicker.20110318.min.js',
 			'libs/jquery.loadmask.min.js',
 			'libs/jquery.colorPicker.20091014.js',
 			'libs/tag-it.20110621.js',
 			'libs/jquery.qtip.20111022.min.js',
 			'libs/jquery.colorhelpers.js',
+			'libs/jquery.timePicker.20110318.min.js',
 			'app.js',
 			);
 
