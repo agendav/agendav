af52b94e93b8aa328cedad7a82e5f9d323e45db6
Jorge López Pérez <jorge@adobo.org>
2012-01-08 21:38:38 +0100

    Use new theme for documentation
    * Sphinx theme proBlue, from Isaac @ Creative Tech: http://www.kellycreativetech.com/Blog/entry/ProBlue_Sphinx_Theme/
    * Added AgenDAV logo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -96,7 +96,7 @@ html_theme_path = ['_themes']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'adc'
+html_theme = 'proBlue'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -112,7 +112,7 @@ html_theme = 'adc'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'agendav_160px.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
