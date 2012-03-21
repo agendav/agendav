be21c33f72fdbc09f4b7b766b6cf5bada4f7a4fb
Jorge López Pérez <jorge@adobo.org>
2012-03-21 13:56:21 +0100

    Change navigation bar colors

--- web/application/views/navbar.php
+++ web/application/views/navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-fixed-top">
  <div class="navbar-inner">
-  <div class="container">
+  <div class="container-fluid">
    <span class="brand"><?php echo $title ?></span>
 <?php 
 if (isset($logged_in)):
