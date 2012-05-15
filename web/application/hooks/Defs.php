c05df9494ac0cadd4d81db37d413983f13cd535a
Jorge López Pérez <jorge@adobo.org>
2012-05-15 19:57:03 +0200

    Update jQuery (1.7.2) and jQuery UI (1.8.20)

--- web/application/hooks/Defs.php
+++ web/application/hooks/Defs.php
@@ -21,8 +21,8 @@
 class Defs {
 	// Leave jQuery and jQuery UI the two first scripts
 	static $jsfiles= array(
-			'libs/jquery-1.7.1.min.js',
-			'libs/jquery-ui-1.8.17.min.js',
+			'libs/jquery-1.7.2.min.js',
+			'libs/jquery-ui-1.8.20.min.js',
 			'libs/jquery.freeow.min.js',
 			'libs/fullcalendar.min.js',
 			'libs/jquery.loadmask.min.js',
