a3ef6ba0aa05be5e4960ae65c55e671337cbd188
Jorge López Pérez <jorge@adobo.org>
2012-06-13 08:52:19 +0200

    Update sessions database to support IPv6

---
+++ web/application/migrations/004_ipv6.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+class Migration_Ipv6 extends CI_Migration {
+	public function up() {
+		// Alter ip address field length
+		echo "Enlarging ip_address field to support IPv6...\n";
+		$this->dbforge->modify_column('sessions',
+				array(
+					'ip_address' => array(
+						'TYPE' => 'varchar(45)',
+						'DEFAULT' => '0',
+						),
+					));
+	}
+
+	public function down() {
+		$this->dbforge->modify_column('sessions',
+				array(
+					'ip_address' => array(
+						'TYPE' => 'varchar(16)',
+						'DEFAULT' => '0',
+						),
+					));
+	}
+}
