f892cca9d05abb406271c3afc2130980f1f993ee
Jorge López Pérez <jorge@adobo.org>
2012-08-06 12:27:33 +0200

    Do not alter user_from when updating shared calendars

--- web/application/models/shared_calendars.php
+++ web/application/models/shared_calendars.php
@@ -171,6 +171,9 @@ class Shared_calendars extends CI_Model {
 		$res = false;
 		if (!is_null($sid)) {
 			$conditions = $data;
+            // user_from doesn't need to be update
+            unset($conditions['user_from']);
+            unset($data['user_from']);
 			unset($conditions['options']);
 			unset($conditions['write_access']);
 
