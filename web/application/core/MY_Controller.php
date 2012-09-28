099bd222c5f10dec431aa0fd3cd422e26e7fa425
Jorge López Pérez <jorge@adobo.org>
2012-09-28 09:13:19 +0200

    Add Pimple and use it

---
+++ web/application/core/MY_Controller.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * Copyright 2012 Jorge López Pérez <jorge@adobo.org>
+ *
+ *  This file is part of AgenDAV.
+ *
+ *  AgenDAV is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  AgenDAV is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AgenDAV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class MY_Controller extends CI_Controller
+{
+    public function __construct() {
+        parent::__construct();
+        $this->container = new Pimple();
+
+        // Classes
+        $this->container['user_class'] = '\AgenDAV\User';
+        $this->container['urlgenerator_class'] = '\AgenDAV\CalDAV\URLGenerator';
+
+        // URLGenerator
+        $this->container['urlgenerator'] = $this->container->share(function($container) {
+            $c = $container['urlgenerator_class'];
+            return new $c(
+                $this->config->item('caldav_server'),
+                $this->config->item('caldav_principal_url'),
+                $this->config->item('caldav_calendar_homeset_template')
+            );
+        });
+
+        // User
+        $this->container['user'] = $this->container->share(function($container) {
+            $c = $container['user_class'];
+            return new $c(
+                $this->session,
+                $this->preferences,
+                $this->encrypt
+            );
+        });
+    }
+}
+
