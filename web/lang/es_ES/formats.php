72ac1ef6f82df1b1995d5a0db0be54024e43787e
Jorge López Pérez <jorge@adobo.org>
2012-10-10 20:51:48 +0200

    Include date formats as a part of every language
    AgenDAV doesn't still use these

---
+++ web/lang/es_ES/formats.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ *  This file is part of AgenDAV.
+ *
+ *  AgenDAV is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  AgenDAV is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AgenDAV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This file contains date and time formats for several parts of AgenDAV
+ * Please, read each comment carefully
+ */
+
+$formats = array();
+
+/*
+ * Full date used on informational messages
+ *
+ * Uses strftime syntax
+ * http://php.net/strftime
+ */
+$formats['full_date_strftime'] = '%a %e de %B de %Y';
+
+/*
+ * Fullcalendar labels
+ *
+ * Use special syntax from Fullcalendar:
+ * http://arshaw.com/fullcalendar/docs/utilities/formatDate/
+ */
+$formats['column_month_fullcalendar'] = 'ddd';
+$formats['column_week_fullcalendar'] = 'ddd d';
+$formats['column_day_fullcalendar'] = 'ddd d \'de\' MMMM';
+$formats['column_table_fullcalendar'] = 'd MMM, yyyy';
+$formats['title_month_fullcalendar'] = 'MMMM yyyy';
+$formats['title_week_fullcalendar'] = "d[ MMM][ yyyy]{ '&#8212;'d MMM yyyy}";
+$formats['title_day_fullcalendar'] = 'dddd d \'de\' MMMM \'de\' yyyy';
+$formats['title_table_fullcalendar'] = 'dddd, MMM d yyyy';
+
