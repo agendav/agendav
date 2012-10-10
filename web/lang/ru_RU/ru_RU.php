9ceea5cce59ed5cc62e3869d9126b66e127f6957
Jorge López Pérez <jorge@adobo.org>
2012-10-10 20:59:43 +0200

    Add ru_RU translation

---
+++ web/lang/ru_RU/ru_RU.php
@@ -0,0 +1,252 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+/*
+ *  This file is part of AgenDAV.
+ *
+ *  AgenDAV is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  AgenDAV is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AgenDAV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$labels = array();
+$messages = array();
+
+// Labels
+$labels['username'] = 'Пользователь';
+$labels['password'] = 'Пароль';
+
+$labels['january'] = 'Январь';
+$labels['february'] = 'Февраль';
+$labels['march'] = 'Март';
+$labels['april'] = 'Апрель';
+$labels['may'] = 'Май';
+$labels['june'] = 'Июнь';
+$labels['july'] = 'Июль';
+$labels['august'] = 'Август';
+$labels['september'] = 'Сентябрь';
+$labels['october'] = 'Октябрь';
+$labels['november'] = 'Ноябрь';
+$labels['december'] = 'Декабрь';
+
+$labels['january_short'] = 'Янв';
+$labels['february_short'] = 'Фев';
+$labels['march_short'] = 'Мар';
+$labels['april_short'] = 'Апр';
+$labels['may_short'] = 'Май';
+$labels['june_short'] = 'Июн';
+$labels['july_short'] = 'Июл';
+$labels['august_short'] = 'Авг';
+$labels['september_short'] = 'Сен';
+$labels['october_short'] = 'Окт';
+$labels['november_short'] = 'Ноя';
+$labels['december_short'] = 'Дек';
+
+$labels['sunday'] = 'Воскресенье';
+$labels['monday'] = 'Понедельник';
+$labels['tuesday'] = 'Вторник';
+$labels['wednesday'] = 'Среда';
+$labels['thursday'] = 'Четверг';
+$labels['friday'] = 'Пятница';
+$labels['saturday'] = 'Суббота';
+
+$labels['sunday_short'] = 'Вс';
+$labels['monday_short'] = 'Пн';
+$labels['tuesday_short'] = 'Вт';
+$labels['wednesday_short'] = 'Ср';
+$labels['thursday_short'] = 'Чт';
+$labels['friday_short'] = 'Пт';
+$labels['saturday_short'] = 'Сб';
+
+$labels['today'] = 'Сегодня';
+$labels['tomorrow'] = 'Завтра';
+$labels['month'] = 'месяц';
+$labels['week'] = 'неделя';
+$labels['day'] = 'день';
+$labels['tableview'] = 'повестка дня';
+$labels['allday'] = 'весь день';
+$labels['choose_date'] = 'Выберите дату';
+
+$labels['thisweek'] = 'Эта неделя';
+$labels['nextweek'] = 'В ближайшую неделю';
+$labels['thismonth'] = 'Этот месяц';
+$labels['nextmonth'] = 'В ближайший месяц';
+$labels['future'] = 'Будущее';
+
+$labels['calendar'] = 'Календарь';
+$labels['location'] = 'Расположение';
+$labels['description'] = 'Описание';
+
+$labels['displayname'] = 'Название';
+$labels['internalname'] = 'Внутреннее имя';
+$labels['optional'] = '(необязательно)';
+$labels['color'] = 'Цвет';
+
+$labels['summary'] = 'Тема';
+$labels['startdate'] = 'Начало';
+$labels['enddate'] = 'Конец';
+$labels['starttime'] = 'Начинается в';
+$labels['endtime'] = 'Заканчивается в';
+$labels['alldayform'] = 'Весь день';
+
+$labels['repetitionexceptions'] = 'Исключения для повторяющихся событий';
+
+$labels['repeat'] = 'Повторять';
+$labels['repeatno'] = 'Без повтора';
+$labels['repeatdaily'] = 'Ежедневно';
+$labels['repeatweekly'] = 'Еженедельно';
+$labels['repeatmonthly'] = 'Ежемесячно';
+$labels['repeatyearly'] = 'Ежегодно';
+
+$labels['repeatcount'] = 'раз';
+$labels['repeatuntil'] = 'до';
+
+$labels['explntimes'] = '%n раз';
+$labels['expluntil'] = 'до %d';
+
+$labels['privacy'] = 'Доступность';
+$labels['public'] = 'Общий';
+$labels['private'] = 'Персональный';
+$labels['confidential'] = 'Только для меня';
+
+$labels['transp'] = 'Состояние';
+$labels['opaque'] = 'Занят';
+$labels['transparent'] = 'Свободен';
+
+$labels['generaloptions'] = 'Основное';
+$labels['repeatoptions'] = 'Повтор';
+$labels['workgroupoptions'] = 'Рабочее';
+$labels['shareoptions'] = 'Общее';
+
+$labels['newcalendar'] = 'Новый календарь';
+$labels['modifycalendar'] = 'Изменить календарь';
+
+$labels['createevent'] = 'Создать событие';
+$labels['editevent'] = 'Редактировать событие';
+$labels['deleteevent'] = 'Удалить событие';
+$labels['deletecalendar'] = 'Удалить календарь';
+$labels['calendars'] = 'Календари';
+$labels['shared_calendars'] = 'Общие календари';
+$labels['refresh'] = 'Обновить';
+$labels['delete'] = 'Удалить';
+$labels['add'] = 'Добавить';
+$labels['close'] = 'Закрыть';
+$labels['save'] = 'Сохранить';
+$labels['create'] = 'Создать';
+$labels['login'] = 'Войти';
+$labels['logout'] = 'Выход';
+$labels['modify'] = 'Изменить';
+$labels['cancel'] = 'Отмена';
+$labels['next'] = 'следующая';
+$labels['previous'] = 'предыдущая';
+$labels['yes'] = 'Да';
+
+$labels['untitled'] = 'Без названия';
+
+$labels['sharewith'] = 'Общий для';
+$labels['currentlysharing'] = 'Общий календарь';
+$labels['publicurl'] = 'URL календаря для CalDAV приложений';
+
+$labels['access'] = 'Доступ';
+$labels['readonly'] = 'Только просмотр';
+$labels['readandwrite'] = 'Просмотр и изменение';
+
+$labels['pastevents'] = 'Прошедшее';
+
+$labels['preferences'] = 'Настройки';
+$labels['return'] = 'Назад';
+
+$labels['hidelist'] = 'Скрыть из списка';
+$labels['defaultcalendar'] = 'Основной календарь';
+
+$labels['toggleallcalendars'] = 'Скрыть/показать все';
+
+$labels['popup'] = 'Всплывающее окно';
+$labels['email'] = 'Email';
+
+$labels['minutes'] = 'минут';
+$labels['hours'] = 'часов';
+$labels['days'] = 'дней';
+$labels['weeks'] = 'недель';
+
+$labels['remindersoptions'] = 'Напоминания';
+$labels['reminder'] = 'Напоминание';
+
+$labels['newreminder'] = 'Новое напоминание:';
+
+$labels['before'] = 'до';
+$labels['after'] = 'после';
+
+$labels['start'] = 'начало';
+$labels['end'] = 'конец';
+
+
+// Messages
+$messages['error_auth'] = 'Неправильное имя пользователя или пароль';
+$messages['error_invaliddate'] = 'Некорректная дата в поле %s';
+$messages['error_invalidtime'] = 'Некорректное время в поле %s';
+$messages['error_denied'] = 'Сервер отклонил запрос (нет прав)';
+$messages['error_notimplemented'] = '%feature: не поддерживается';
+$messages['error_startgreaterend'] = 'Дата завершения должна быть старше даты начала';
+$messages['error_bogusrepeatrule'] = 'Ошибка в параметрах повтора';
+$messages['error_internalgen'] = 'Ошибка при создании внутреннего календаря';
+$messages['error_internalcalnameinuse'] = 'Внутреннее имя уже используется';
+
+$messages['info_confirmcaldelete'] = 'Вы уверены, что хотите удалить календарь?';
+$messages['info_confirmeventdelete'] = 'Вы уверены что хотите удалить эти события?';
+$messages['info_permanentremoval'] = 'Информация будет безвозвратно удалена';
+$messages['info_repetitivedeleteall'] = 'Все повторы этого события будут удалены';
+$messages['info_sharedby'] = '%user предоставил Вам доступ к этому календарю';
+$messages['info_shareexplanation'] = 'Вы можете предоставить доступ
+к этому календарю другим пользователям.
+Укажите их имена, разделяя запятыми или пробелами';
+$messages['info_notshared'] = 'Этот календарь не является общим';
+$messages['info_noreminders'] = 'Нет напоминаний';
+$messages['error_sessexpired'] = 'Сессия завершена';
+$messages['error_loginagain'] = 'Войдите снова.';
+
+$messages['error_modfailed'] = 'Ошибка при сохранении';
+$messages['error_loadevents'] = 'Ошибка при загрузке событий %cal календаря';
+$messages['error_sessrefresh'] = 'Ошибка при обновлении сессии';
+$messages['error_internal'] = 'Внутренняя ошибка';
+$messages['error_genform'] = 'Ошибка при создании формы';
+$messages['error_invalidinput'] = 'Некорректное значение';
+$messages['error_caldelete'] = 'Ошибка при удалении календаря';
+
+$messages['overlay_synchronizing'] = 'Синхронизация событий...';
+$messages['overlay_loading_dialog'] = 'Загрузка формы...';
+$messages['overlay_sending_form'] = 'Отправка формы...';
+$messages['overlay_loading_calendar_list'] = 'Загрузка списка календарей...';
+$messages['error_loading_dialog'] = 'Ошибка загрузки формы';
+
+$messages['error_oops'] = 'Непредвиденная ситуация';
+$messages['error_interfacefailure'] = 'Ошибка интерфейса';
+$messages['error_current_event_not_loaded'] = 'Событие недоступно';
+
+$messages['error_event_not_deleted'] = 'Ошибка при удалении события';
+$messages['error_loading_calendar_list'] = 'Ошибка при чтении списка календарей';
+$messages['notice_no_calendars'] = 'Нет доступных календарей';
+$messages['info_repetition_human'] = 'Событие повторяется %explanation';
+$messages['info_repetition_unparseable'] = 'Событие настроено на повтор по правилам, непонятным для AgenDAV. DEBUG: ';
+$messages['error_calendarnotfound'] = 'Некорректный календарь %calendar';
+$messages['error_eventnotfound'] = 'Объект не найден';
+$messages['error_eventchanged'] = 'Объект был изменён другим пользователем. Обновите страницу.';
+$messages['error_unknownhttpcode'] = 'Неизвестная ошибка, HTTP код=%res';
+$messages['error_internalcalnamemissing'] = 'Укажите внутреннее имя календаря';
+$messages['error_calname_missing'] = 'Укажите название календаря';
+$messages['error_calcolor_missing'] = 'Выберите цвет';
+$messages['error_mkcalendar'] = 'Ошибка создания календаря. Проверьте параметры.';
+$messages['error_shareunknownusers'] = 'Пользователь, которого Вы указали не существует';
+
+$messages['help_defaultcalendar'] = 'Новые события будут созданы по умолчанию в этом календаре.';
+
+$messages['info_prefssaved'] = 'Настройки сохранены';
+
+$messages['more_events'] = '+ %count events';
