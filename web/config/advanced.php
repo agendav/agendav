2521b32c0c95829d908310ddaf2470b88f07f4bb
Jorge López Pérez <jorge@adobo.org>
2012-10-10 13:27:48 +0200

    Rename CSRF cookie/token
    Old names were the default ones

--- web/config/advanced.php
+++ web/config/advanced.php
@@ -198,8 +198,8 @@ $config['global_xss_filtering'] = TRUE;
 | 'csrf_expire' = The number in seconds the token should expire.
 */
 $config['csrf_protection'] = TRUE;
-$config['csrf_token_name'] = 'csrf_test_name';
-$config['csrf_cookie_name'] = 'csrf_cookie_name';
+$config['csrf_token_name'] = 'check_csrf';
+$config['csrf_cookie_name'] = 'csrf_agendav';
 $config['csrf_expire'] = 7200;
 
 /*
