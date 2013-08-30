825b48d99f66edec5670ce1504de9b9f19fb9d25
Simon Schick <simonsimcity@gmail.com>
2013-08-30 14:48:42 +0200

    Simplified updating the language to user preferences

--- web/application/models/i18n.php
+++ web/application/models/i18n.php
@@ -34,16 +34,15 @@ class I18n extends CI_Model {
         // Load language relations file
         $this->config->load('languages');
 
-        /** @var \AgenDAV\User $user */
-        $user = $this->container['user'];
-        $prefs = $user->getPreferences()->getAll();
 
         $this->lang_path = APPPATH . '../lang';
         $this->langname = $this->config->item('default_language');
 
+        /** @var \AgenDAV\User $user */
+        $user = $this->container['user'];
         // Overwrite default setting by users preferences
-        if (isset($prefs['language']))
-            $this->langname = $prefs['language'];
+        if ($user->getPreferences()->language !== null)
+            $this->langname = $user->getPreferences()->language;
 
         if (!is_dir($this->lang_path)) {
             log_message('ERROR', 'Language path is not a directory');
