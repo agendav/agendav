6c031f0f892f7bea14f9d8eb6e053b86ae7ec22a
Jorge López Pérez <jorge@adobo.org>
2014-03-28 10:53:00 +0100

    Add unit tests for the HTTP client

---
+++ tests/AgenDAV/Http/ClientTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace AgenDAV\Http;
+
+use AgenDAV\Http\Client;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Subscriber\Mock;
+use GuzzleHttp\Message\Response;
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddAndSetHeaders()
+    {
+        $guzzle = new GuzzleClient();
+        $client = new Client($guzzle);
+
+        $this->assertNull($client->getHeader('Non-existing'));
+
+        $client->setHeader('Test-Header', 'Test-Value');
+        $this->assertEquals($client->getHeader('Test-Header'), 'Test-Value');
+
+        // Overwrite old header
+        $client->setHeader('Test-Header', 'Second value');
+        $this->assertEquals($client->getHeader('Test-Header'), 'Second value');
+
+        // Add a value to the header
+        $client->addHeader('Test-Header', 'Additional value');
+        $this->assertEquals(
+            $client->getHeader('Test-Header'),
+            array('Second value', 'Additional value')
+        );
+
+        // Add one more value to the header
+        $client->addHeader('Test-Header', 'One more additional value');
+        $this->assertEquals(
+            $client->getHeader('Test-Header'),
+            array('Second value', 'Additional value', 'One more additional value')
+        );
+
+        // addHeader for non-defined header
+        $client->addHeader('New-Header', 'new');
+        $this->assertEquals($client->getHeader('New-Header'), 'new');
+    }
+
+
+    public function testHeadersSent()
+    {
+        $guzzle = new GuzzleClient();
+        $response_mock = new Mock(array(
+            'HTTP/1.1 202 OKr\nContent-Length: 0\r\n\r\n'
+        ));
+        $guzzle->getEmitter()->attach($response_mock);
+
+        $client = new Client($guzzle);
+        $client->setHeader('Test-Header', 'Test value');
+        $client->request('GET', '/');
+
+        $headers = $client->getLastRequest()->getHeaders();
+        $this->assertArrayHasKey('Test-Header', $headers);
+        $this->assertEquals($headers['Test-Header'], array('Test value'));
+    }
+
+    /**
+     * Should not throw any exceptions at all
+     */
+    public function testNoExceptions()
+    {
+        $guzzle = new GuzzleClient();
+        $response_mock = new Mock(array(
+            'HTTP/1.1 404 Not foundr\nContent-Length: 0\r\n\r\n'
+        ));
+        $guzzle->getEmitter()->attach($response_mock);
+
+        $client = new Client($guzzle);
+        $client->setHeader('Test-Header', 'Test value');
+    }
+}
