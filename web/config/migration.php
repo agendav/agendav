67eb3b88aadf7715af784e01c9da513489b054e7
Jorge López Pérez <jorge@adobo.org>
2014-08-14 17:22:01 +0200

    Remove ancient "No direct access to this script"

--- web/config/migration.php
+++ web/config/migration.php
@@ -1,4 +1,4 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
 /*
 |--------------------------------------------------------------------------
 | Enable/Disable Migrations
