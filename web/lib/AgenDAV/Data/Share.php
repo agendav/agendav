7ab6fa6f2e0dd9d67e0fae208c25fce573811082
Jorge López Pérez <jorge@adobo.org>
2014-09-12 20:37:32 +0200

    Add a shares repository
    Includes an implementation based on Doctrine ORM

---
+++ web/lib/AgenDAV/Data/Share.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace AgenDAV\Data;
+
+/*
+ * Copyright 2014 Jorge López Pérez <jorge@adobo.org>
+ *
+ *  This file is part of AgenDAV.
+ *
+ *  AgenDAV is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  AgenDAV is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AgenDAV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/**
+ * Holds a shared calendar information
+ */
+
+/**
+ * @Entity
+ * @Table(name="shares")
+ */
+class Share
+{
+    /**
+     * @Id @Column(type="integer")
+     * @GeneratedValue
+     */
+    private $sid;
+
+    /** @Column(type="string") */
+    private $grantor;
+
+    /** @Column(type="string") */
+    private $path;
+
+    /** @Column(type="string") */
+    private $grantee;
+
+    /** @Column(type="array") */
+    private $options = array();
+
+    /** @Column(type="boolean") */
+    private $rw;
+
+    /*
+     * Getter for sid
+     */
+    public function getSid()
+    {
+        return $this->sid;
+    }
+
+    /*
+     * Getter for grantor
+     */
+    public function getGrantor()
+    {
+        return $this->grantor;
+    }
+
+    /*
+     * Setter for grantor
+     */
+    public function setGrantor($grantor)
+    {
+        $this->grantor = $grantor;
+        return $this;
+    }
+
+    /*
+     * Getter for path
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /*
+     * Setter for path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    /*
+     * Getter for grantee
+     */
+    public function getGrantee()
+    {
+        return $this->grantee;
+    }
+
+    /*
+     * Setter for grantee
+     */
+    public function setGrantee($grantee)
+    {
+        $this->grantee = $grantee;
+        return $this;
+    }
+
+    /*
+     * Returns true if a share allows modifications
+     */
+    public function isWritable()
+    {
+        return $this->rw === true;
+    }
+
+    /*
+     * Setter for rw
+     *
+     * @param bool $rw  Whether this share allows modifications or not
+     */
+    public function setWritePermission($rw)
+    {
+        $this->rw = $rw;
+        return $this;
+    }
+
+    /**
+     * Returns all properties/options set on the shared resource
+     *
+     * @return Array
+     */
+    public function getProperties()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Returns a property/option set on this resource, or null if it
+     * is not set
+     */
+    public function getProperty($name)
+    {
+        return array_key_exists($name, $this->options) ?  $this->options[$name] : null;
+    }
+
+    /**
+     * Sets a property/option on this resource
+     */
+    public function setProperty($name, $value)
+    {
+        $this->options[$name] = $value;
+        return $this;
+    }
+
+    public function to_json() {
+        return json_encode($this->options);
+    }
+}
---
+++ web/lib/AgenDAV/Repositories/DoctrineOrmSharesRepository.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+ * Copyright 2014 Jorge López Pérez <jorge@adobo.org>
+ *
+ *  This file is part of AgenDAV.
+ *
+ *  AgenDAV is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  AgenDAV is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AgenDAV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace AgenDAV\Repositories;
+
+use Doctrine\ORM\EntityManager;
+use AgenDAV\Data\Share;
+
+
+/**
+ * Implements the shares repository using Doctrine ORM
+ *
+ * @author Jorge López Pérez <jorge@adobo.org>
+ */
+class DoctrineOrmSharesRepository implements SharesRepository
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+
+    /**
+     * @param Doctrine\ORM\EntityManager Entity manager
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Returns all calendars shared with a user
+     *
+     * @param string $username  User name
+     * @return Array Array of Share's
+     */
+    public function getSharesFor($username)
+    {
+        $shares = $this->em->getRepository('AgenDAV\Data\Share')
+            ->findBy(['grantee' => $username]);
+
+        return $shares;
+    }
+
+    /**
+     * Returns all grants that have been given to a calendar
+     *
+     * @param string $path  Path to the calendar
+     * @return Array Array of Share's
+     */
+    public function getSharesOnCalendar($path)
+    {
+        $shares = $this->em->getRepository('AgenDAV\Data\Share')
+            ->findBy(['path' => $path]);
+
+        return $shares;
+    }
+
+    /**
+     * Stores a grant on the database
+     *
+     * @param AgenDAV\Data\Share $share  Share object
+     */
+    public function save(Share $share)
+    {
+        $this->em->persist($share);
+        $this->em->flush();
+    }
+
+    /**
+     * Removes a grant for a calendar
+     *
+     * @param AgenDAV\Data\Share $share  Share object
+     */
+    public function remove(Share $share)
+    {
+        $this->em->remove($share);
+        $this->em->flush();
+    }
+}
