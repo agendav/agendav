4039b2824b42d77bc612f37021df1e9f37ecb452
Jorge López Pérez <jorge@adobo.org>
2014-11-11 13:28:45 +0100

    Add a test for Calendar::getAllProperties

--- tests/AgenDAV/Data/CalendarTest.php
+++ tests/AgenDAV/Data/CalendarTest.php
@@ -20,4 +20,17 @@ class CalendarTest extends \PHPUnit_Framework_TestCase
         $c->setProperty('url',  '/should_not_change');
     }
 
+    public function testGetAllProperties()
+    {
+        $c = new Calendar('/path');
+        $c->setProperty(Calendar::DISPLAYNAME,  'Test');
+        $c->setProperty('{urn:fake}attr', 'value');
+
+        $this->assertEquals(
+            $c->getAllProperties(),
+            [Calendar::DISPLAYNAME => 'Test', '{urn:fake}attr' => 'value']
+        );
+
+    }
+
 }
