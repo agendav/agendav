8211a6143e3cdf5f9198a4b9bbba449c39c24aa8
Jorge López Pérez <jorge@adobo.org>
2014-11-14 09:56:51 +0100

    Move REPORT filters to their own class
    We'll need this to filter by event uid too

---
+++ tests/AgenDAV/CalDAV/TimeRangeFilterTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace AgenDAV\CalDAV;
+
+class TimeRangeFilterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGeneration()
+    {
+        $document = new \DOMDocument('1.0', 'UTF-8');
+
+        $time_range = new TimeRangeFilter(
+            '20141114T000000Z',
+            '20141115T000000Z'
+        );
+
+        $time_range_element = $time_range->generateFilterXML($document);
+        $time_range_element->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:C', 'urn:ietf:params:xml:ns:caldav');
+
+        $document->appendChild($time_range_element);
+        $expected = <<<EOXML
+<?xml version="1.0" encoding="UTF-8"?>
+<C:time-range xmlns:C="urn:ietf:params:xml:ns:caldav" start="20141114T000000Z" end="20141115T000000Z"/>
+EOXML;
+
+        $this->assertXmlStringEqualsXmlString($expected, $document->saveXML());
+    }
+}
---
+++ web/src/CalDAV/TimeRangeFilter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AgenDAV\CalDAV;
+
+/*
+ * Copyright 2014 Jorge López Pérez <jorge@adobo.org>
+ *
+ *  This file is part of AgenDAV.
+ *
+ *  AgenDAV is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  AgenDAV is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AgenDAV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * <time-range> filter for REPORTs
+ */
+class TimeRangeFilter implements ComponentFilter
+{
+    protected $start;
+
+    protected $end;
+
+    /**
+     * @param string $start ISO8601 based time string, in UTC
+     * @param string $end ISO8601 based time string, in UTC
+     */
+    public function __construct($start, $end)
+    {
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    /**
+     * Returns a DOMElement cotaining this filter
+     *
+     * @param \DOMDocument $document Initial DOMDocument, required to
+     *                               generate a valid \DOMElement
+     * @result \DOMElement $element
+     */
+    public function generateFilterXML(\DOMDocument $document)
+    {
+        $time_range = $document->createElement('C:time-range');
+        $time_range->setAttribute('start', $this->start);
+        $time_range->setAttribute('end', $this->end);
+
+        return $time_range;
+    }
+}
+
