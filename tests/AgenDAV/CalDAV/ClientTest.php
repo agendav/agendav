897cecbbd785932bd2df0f06d4931352f53f11fc
Jorge López Pérez <jorge@adobo.org>
2014-11-19 21:04:58 +0100

    Add more tests for CalDAV client
    Changed the testing approach by mocking Http Client

--- tests/AgenDAV/CalDAV/ClientTest.php
+++ tests/AgenDAV/CalDAV/ClientTest.php
@@ -2,10 +2,9 @@
 
 namespace AgenDAV\CalDAV;
 
-use GuzzleHttp\Client as GuzzleHttp;
-use GuzzleHttp\Subscriber\Mock;
-use GuzzleHttp\Subscriber\History;
+use Mockery as m;
 use GuzzleHttp\Message\Response;
+use GuzzleHttp\Stream\Stream;
 use AgenDAV\Http\Client as HttpClient;
 use AgenDAV\XML\Generator;
 use AgenDAV\XML\Parser;
@@ -15,31 +14,55 @@ use AgenDAV\XML\Parser;
  */
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var GuzzleHttp\Subscriber\History */
-    protected $history;
-
+    /** @var \AgenDAV\Http\Client */
     protected $http_client;
+
+    /** @var \AgenDAV\XML\Generator */
+    protected $xml_generator;
+
+    /** @var \AgenDAV\XML\Parser */
+    protected $xml_parser;
+
+    public function setUp()
+    {
+        $this->http_client = m::mock('AgenDAV\Http\Client');
+        $this->xml_generator = m::mock('AgenDAV\XML\Generator');
+        $this->xml_parser = m::mock('AgenDAV\XML\Parser');
+    }
+
+    public function testCantAuthenticate()
+    {
+        // #1 Test an authentication failure
+        $this->http_client->shouldReceive('request')
+            ->with('OPTIONS', '')
+            ->andThrow(m::mock('\GuzzleHttp\Exception\ClientException'))
+            ->once();
+        $caldav_client = $this->createCalDAVClient();
+
+        $this->assertFalse($caldav_client->canAuthenticate(), 'canAuthenticate() works on 4xx/5xx');
+    }
+
+    public function testCantAuthenticateNotCalDAV()
+    {
+        // #2 Test a non CalDAV server
+        $response = new Response(200, []);
+        $this->http_client->shouldReceive('request')
+            ->with('OPTIONS', '')
+            ->andReturn($response)
+            ->once();
+        $caldav_client = $this->createCalDAVClient();
+        $this->assertFalse($caldav_client->canAuthenticate(), 'canAuthenticate() works on non CalDAV servers');
+    }
 
     public function testCanAuthenticate()
     {
-        $responses = [];
-
-        // #1 Test an authentication failure
-        $responses[] = new Response(401);
-
-        // #2 Test a non CalDAV server
-        $responses[] = new Response(200, []);
-
         // #3 Test a valid authentication
-        $responses[] = new Response(200, ['DAV' => '1, 3, extended-mkcol, access-control, calendarserver-principal-property-search, calendar-access, calendar-proxy']);
-
-        $caldav_client = $this->getMockedClient($responses);
-
-        // #1
-        $this->assertFalse($caldav_client->canAuthenticate(), 'canAuthenticate() works on 401');
-        // #2
-        $this->assertFalse($caldav_client->canAuthenticate(), 'canAuthenticate() works on non CalDAV servers');
-        // #3
+        $response = new Response(200, ['DAV' => '1, 3, extended-mkcol, access-control, calendarserver-principal-property-search, calendar-access, calendar-proxy']);
+        $this->http_client->shouldReceive('request')
+            ->with('OPTIONS', '')
+            ->andReturn($response)
+            ->once();
+        $caldav_client = $this->createCalDAVClient();
         $this->assertTrue(
             $caldav_client->canAuthenticate(),
             'canAuthenticate() does not work when authentication is successful on a CalDAV server'
@@ -47,21 +70,72 @@ class ClientTest extends \PHPUnit_Framework_TestCase
     }
 
 
-    protected function getMockedClient(Array $responses)
+    /** @expectedException \UnexpectedValueException */
+    public function testGetCurrentUserPrincipalNotFound()
     {
-        $guzzle = new GuzzleHttp();
-        $mock = new Mock($responses);
-        $guzzle->getEmitter()->attach($mock);
+        $this->xml_generator->shouldReceive('propfindBody')
+            ->andReturn('returned_body_from_xml_generator')
+            ->once();
+        $response = new Response(207, [], Stream::factory('fake_response'));
+        $this->http_client->shouldReceive('setHeader')
+            ->with('Depth', 0)
+            ->once()
+            ->ordered();
+        $this->http_client->shouldReceive('setContentTypeXML')
+            ->once()
+            ->ordered();
+        $this->http_client->shouldReceive('request')
+            ->with('PROPFIND', '', 'returned_body_from_xml_generator')
+            ->andReturn($response)
+            ->once()
+            ->ordered();
+        $this->xml_parser->shouldReceive('extractPropertiesFromMultistatus')
+            ->with('fake_response', true)
+            ->andReturn([])
+            ->once();
 
-        $this->history = new History();
-        $guzzle->getEmitter()->attach($this->history);
+        $caldav_client = $this->createCalDAVClient();
 
-        $this->http_client = new HttpClient($guzzle);
+        $caldav_client->getCurrentUserPrincipal();
+    }
 
-        return new Client(
-            $this->http_client,
-            new Generator(),
-            new Parser()
+    public function testGetCurrentUserPrincipalSuccess()
+    {
+        $this->xml_generator->shouldReceive('propfindBody')
+            ->andReturn('returned_body_from_xml_generator')
+            ->once();
+        $response = new Response(207, [], Stream::factory('fake_response'));
+        $this->http_client->shouldReceive('setHeader')
+            ->with('Depth', 0)
+            ->once()
+            ->ordered();
+        $this->http_client->shouldReceive('setContentTypeXML')
+            ->once()
+            ->ordered();
+        $this->http_client->shouldReceive('request')
+            ->with('PROPFIND', '', 'returned_body_from_xml_generator')
+            ->andReturn($response)
+            ->once()
+            ->ordered();
+        $this->xml_parser->shouldReceive('extractPropertiesFromMultistatus')
+            ->with('fake_response', true)
+            ->andReturn(['/principals/fake'])
+            ->once();
+
+        $caldav_client = $this->createCalDAVClient();
+
+        $this->assertEquals(
+            '/principals/fake',
+            $caldav_client->getCurrentUserPrincipal()
         );
+    }
+
+
+    /**
+     * Create CalDAV client using other mocks
+     */
+    protected function createCalDAVClient()
+    {
+        return new Client($this->http_client, $this->xml_generator, $this->xml_parser);
     }
 }
