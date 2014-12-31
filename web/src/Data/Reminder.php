b71b151279058ead09bd60225a5a4e3a24d7da4e
Jorge López Pérez <jorge@adobo.org>
2014-12-31 15:18:54 +0100

    Add a new basic Reminder class

--- web/src/Data/Reminder.php
+++ web/src/Data/Reminder.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
 namespace AgenDAV\Data;
 
 /*
- * Copyright 2012 Jorge López Pérez <jorge@adobo.org>
+ * Copyright 2014 Jorge López Pérez <jorge@adobo.org>
  *
  *  This file is part of AgenDAV.
  *
@@ -25,110 +25,45 @@ use \AgenDAV\DateHelper;
 
 class Reminder
 {
-    public $type, $order = FALSE;
-    public $is_absolute;
-    public $before;
-    public $qty, $interval;
-    public $relatedStart;
-    public $absdatetime, $tdate, $ttime;
+    /**
+     * Stores the position for this reminder in the original
+     * event
+     *
+     * @var integer
+     */
+    protected $position;
 
-    public static $intervals = array(
-            'week' => 10080,
-            'day' => 1440,
-            'hour' => 60,
-            'min' => 1,
-            );
+    /**
+     * @var \DateInterval
+     */
+    protected $when;
 
-    public function __construct() {
-        // TODO add more types
-        $this->type = 'DISPLAY';
-    }
-
-    public static function createFrom($when) {
-        $new_reminder = new Reminder();
-        if (is_array($when)) {
-            // Related to start/end
-            $new_reminder->is_absolute = FALSE;
-            $new_reminder->parse_trigger($when);
-        } else {
-            // Absolute
-            $new_reminder->is_absolute = TRUE;
-            $new_reminder->absdatetime = $when;
-        }
-
-        return $new_reminder;
-    }
-
-
-    public function parse_trigger($trigger) {
-        $this->before = $trigger['before'];
-        $this->relatedStart = $trigger['relatedStart'];
-        $this->approx_trigger($trigger);
-    }
-
-    private function approx_trigger($trigger) {
-        $minutes = 0;
-        foreach (self::$intervals as $u => $m) {
-            if (isset($trigger[$u])) {
-                $minutes += $trigger[$u]*$m;
-            }
-        }
-
-        if ($minutes == 0) {
-            $use_unit = 'min';
-            // Fix 'before'
-            $this->before = TRUE;
-        } else {
-            // Decide a measure
-            $use_unit = '';
-            foreach (self::$intervals as $unit => $q) {
-                if ($minutes % $q == 0) {
-                    $use_unit = $unit;
-                    break;
-                }
-            }
-        }
-
-        $this->qty = $minutes/self::$intervals[$use_unit];
-        $this->interval = $use_unit;
+    /**
+     * @param \DateInterval $when
+     * @param integer $position
+     */
+    public function __construct($when, $position = null)
+    {
+        $this->when = clone $when;
+        $this->position = $position;
     }
 
     /**
-     * Assigns the trigger, action and description for the given VALARM
-     component
+     * Parses an input array and returns a Reminder
+     *
+     * @param array $input Key-value based array. Expected keys are:
+     *                     - count: number of <units>
+     *                     - unit: one of minutes, hours or days
+     *                     - position (optional)
+     * @return AgenDAV\Data\Reminder
      */
-    public function assign_properties(&$valarm) {
-        if ($this->is_absolute) {
-            $valarm->setProperty('trigger',
-                    DateHelper::DateTimeToiCalendar($this->absdatetime, 'DATE-TIME'),
-                    array('VALUE' => 'DATE-TIME'));
-        } else {
-            $valarm->setProperty('trigger',
-                    array(
-                        $this->interval => $this->qty,
-                        'relatedStart' => $this->relatedStart,
-                        'before' => $this->before,
-                        ));
-        }
+    public static function createFromInput(array $input)
+    {
+        $string = $input['count'] . ' ' . $input['unit'];
+        $interval = \DateInterval::createFromDateString($string);
 
-        $valarm->setProperty('action', $this->type);
-        // TODO store description
-        $valarm->setProperty('description', 'AgenDAV');
+        $position = isset($input['position']) ? $input['position'] : null;
 
-        log_message('INTERNALS', 'Returning VALARM ' .
-                $valarm->createComponent($x));
-
-        return $valarm;
-    }
-
-
-    public function __toString() {
-        if ($this->is_absolute) {
-            return 'R[' . $this->absdatetime->format('c') . ']';
-        } else {
-            return 'R[' . $this->qty . ' ' . $this->interval .
-                ' ' . ($this->before ? 'before' : 'after') . 
-                ' ' . ($this->relatedStart ? 'start' : 'end') . ']';
-        }
+        return new self($interval, $position);
     }
 }
