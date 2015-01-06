f1ad252881d52c1a5af179e60278e84a6fcc47f2
Jorge López Pérez <jorge@adobo.org>
2015-01-06 11:36:42 +0100

    Fix unreproducible RRULE detection
    Now AgenDAV is capable of detecting which RRULEs can't be exactly reproduced
    using its interface

--- web/public/js/repeat-form.js
+++ web/public/js/repeat-form.js
@@ -61,6 +61,14 @@ AgenDAVRepeat.handleForm = function handleForm($form) {
     var new_rrule = AgenDAVRepeat.generateRRule(
         $form.find('input,select').serializeArray()
     );
+
+    // Repeat was set to none
+    if (new_rrule === null) {
+      $('#rrule').val('');
+      $('#repeat_explanation').html('');
+      return;
+    }
+
     $('#rrule').val(new_rrule.toString());
     $('#repeat_explanation').html(
         AgenDAVRepeat.explainRRule(new_rrule)
@@ -76,7 +84,7 @@ AgenDAVRepeat.handleForm = function handleForm($form) {
  * Generates a RRule based on the form contents
  *
  * @param Object data Form data from serializeArray()
- * @return RRule object
+ * @return RRule|null Repeat rule, or null if recurrence is disabled
  */
 AgenDAVRepeat.generateRRule = function generateRRule(data) {
   var frequency = -1;
@@ -124,7 +132,7 @@ AgenDAVRepeat.generateRRule = function generateRRule(data) {
       options.bymonthday = value;
     }
 
-    if (field.name === 'repeat_interval') {
+    if (field.name === 'repeat_interval' && value !== '1') {
       options.interval = value;
     }
 
@@ -151,8 +159,12 @@ AgenDAVRepeat.generateRRule = function generateRRule(data) {
     return rrule_original;
   }
 
-  return new RRule(options);
+  // Empty RRULE?
+  if (options.freq === undefined) {
+    return null;
+  }
 
+  return new RRule(options);
 };
 
 /**
@@ -320,6 +332,15 @@ AgenDAVRepeat.setRepeatRuleOnForm = function setRepeatRuleOnForm(rrule, form) {
     }
 
     if (param === 'byweekday') {
+      // RRule.js bug. Documentation states:
+      //  Currently, rule.options.byweekday isn't equal to 
+      //  rule.origOptions.byweekday (which is an inconsistency).
+      // This seems to happen for example when byweekday has a 
+      // "last X of month" format (e.g. -2MO)
+
+      if (value === null) {
+        value = rrulejs.origOptions[param];
+      }
       for (var i=0;i<value.length;i++) {
         var label = AgenDAVRepeat.getLabelForByDay(value[i]);
         form.find('.container_repeat_by_day [value=' + label + ']').prop('checked', true);
@@ -336,11 +357,15 @@ AgenDAVRepeat.setRepeatRuleOnForm = function setRepeatRuleOnForm(rrule, form) {
   $('#repeat_frequency').trigger('change');
 
   // Does this generated RRULE match the original one?
-  var generated_rrule = $('#rrule').val();
-  if (generated_rrule !== rrule) {
+  var generated_rrule = RRule.fromString($('#rrule').val());
+  var generated_description = generated_rrule.toText();
+  var original_description = rrulejs.toText();
+
+  if (generated_description !== original_description) {
     $('#repeat_frequency').prepend(
         '<option value="keep-original">'+ t('labels', 'keep_rrule') +'</option>'
     );
+    $('#repeat_warning_rrule_unreproducible').show();
     $('#repeat_frequency').val('keep-original');
     $('#repeat_frequency').trigger('change');
   }
