a46d4fcc2f29a0b8d5290a3a1a526a2fb2dad87d
Jorge López Pérez <jorge@adobo.org>
2015-01-06 11:47:02 +0100

    Remove server-side recurrence classes
    They are managed now client-side!

--- web/config/autoload.php
+++ web/config/autoload.php
@@ -53,7 +53,7 @@ $autoload['packages'] = array();
 */
 
 $autoload['libraries'] = array('encrypt',
-    'timezonemanager', 'dates', 'recurrence', 'icshelper');
+    'timezonemanager', 'dates', 'icshelper');
 
 
 /*
