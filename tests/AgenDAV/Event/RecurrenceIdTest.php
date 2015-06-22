cfa21aef885cea4250a1a1c015ac03d655edafe6
Jorge López Pérez <jorge@adobo.org>
2015-06-22 17:50:42 +0200

    Add a new test for the RecurrenceId class

--- tests/AgenDAV/Event/RecurrenceIdTest.php
+++ tests/AgenDAV/Event/RecurrenceIdTest.php
@@ -29,4 +29,18 @@ class RecurrenceIdTest extends \PHPUnit_Framework_TestCase
         $recurrence_id = RecurrenceId::buildFromString('20150617T173600Z');
         $this->assertTrue($this->datetime == $recurrence_id->getDateTime());
     }
+
+    public function testMatchesDateTime()
+    {
+        $recurrence_id = new RecurrenceId($this->datetime);
+        $this->assertTrue($recurrence_id->matchesDateTime($this->datetime));
+
+        // Test with a different timezone
+        $new_datetime = clone $this->datetime;
+        $new_datetime->setTimeZone(new \DateTimeZone('Africa/Asmara'));
+        $this->assertTrue(
+            $recurrence_id->matchesDateTime($new_datetime),
+            'matchesDateTime() can\'t handle different time zones'
+        );
+    }
 }
