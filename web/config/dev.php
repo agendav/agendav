df8c34ede35b875002ce460260ed08e69b993931
Jorge López Pérez <jorge@adobo.org>
2015-07-30 21:13:02 +0200

    Disable twig cache on development environment
    Should be disabled when developing AgenDAV

--- web/config/dev.php
+++ web/config/dev.php
@@ -8,6 +8,11 @@ require __DIR__ . '/prod.php';
 $app['debug'] = true;
 $app['http.debug'] = true;
 
+// Disable twig cache
+$twig_options = $app['twig.options'];
+unset($twig_options['cache']);
+$app['twig.options'] = $twig_options;
+
 $app->register(new WebProfilerServiceProvider(), [
     'profiler.cache_dir' => '/tmp',
 ]);
