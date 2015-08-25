786d195b7c2d1ee5a6684f5f40726e5cffa2df9c
Jorge López Pérez <jorge@adobo.org>
2015-08-25 19:30:10 +0200

    Add more tests for VObjectBuilder

--- tests/AgenDAV/Event/Builder/VObjectBuilderTest.php
+++ tests/AgenDAV/Event/Builder/VObjectBuilderTest.php
@@ -9,9 +9,13 @@ use Mockery as m;
 
 class VObjectBuilderTest  extends \PHPUnit_Framework_TestCase
 {
+    const INPUT_DATETIME_FORMAT = 'Y-m-d\TH:i:s.u\Z';
+
     protected $utc;
 
     protected $timezone;
+
+    protected $builder;
 
     public function __construct()
     {
@@ -19,23 +23,24 @@ class VObjectBuilderTest  extends \PHPUnit_Framework_TestCase
         $this->timezone = new \DateTimeZone('Europe/Madrid');
     }
 
+    public function setUp()
+    {
+        $this->builder = new VObjectBuilder($this->timezone);
+    }
+
 
     public function testCreateEvent()
     {
-        $builder = new VObjectBuilder($this->utc);
-
-        $event = $builder->createEvent('ABCDEFG');
+        $event = $this->builder->createEvent('ABCDEFG');
 
         $this->assertEquals('ABCDEFG', $event->getUid());
     }
 
     public function testCreateEventInstanceWithNoRecurrenceId()
     {
-        $builder = new VObjectBuilder($this->timezone);
+        $event = $this->builder->createEvent('ABCDEFG');
 
-        $event = $builder->createEvent('ABCDEFG');
-
-        $instance = $builder->createEventInstanceFor($event);
+        $instance = $this->builder->createEventInstanceFor($event);
 
         $this->assertEquals('ABCDEFG', $instance->getUid());
         $this->assertNull($instance->getRecurrenceId());
@@ -43,9 +48,7 @@ class VObjectBuilderTest  extends \PHPUnit_Framework_TestCase
 
     public function testCreateEventInstanceWithRecurrenceId()
     {
-        $builder = new VObjectBuilder($this->timezone);
-
-        $event = $builder->createEvent('ABCDEFG');
+        $event = $this->builder->createEvent('ABCDEFG');
         $instance = $event->createEventInstance();
         $instance->setStart(new \DateTime('2015-08-25 18:10:00', $this->timezone));
         $instance->setSummary('Test event');
@@ -53,11 +56,147 @@ class VObjectBuilderTest  extends \PHPUnit_Framework_TestCase
         $event->storeInstance($instance);
 
         $recurrence_id = RecurrenceId::buildFromString('20150825T171000Z');
-        $exception = $builder->createEventInstanceFor($event, $recurrence_id);
+        $exception = $this->builder->createEventInstanceFor($event, $recurrence_id);
 
         $this->assertEquals(
             $recurrence_id,
             $exception->getRecurrenceId()
+        );
+    }
+
+
+    public function testCreateEventInstanceWithInputMostProperties()
+    {
+        $event = $this->builder->createEvent('ABCDEFG');
+
+        $start = new \DateTime('2015-08-25 16:34:00', $this->utc);
+        $end = new \DateTime('2015-08-25 17:34:00', $this->utc);
+
+        $input = [
+            'start' => $start->format(self::INPUT_DATETIME_FORMAT),
+            'end' => $end->format(self::INPUT_DATETIME_FORMAT),
+            'summary' => 'My test event',
+            'location' => 'My test location',
+            'description' => 'My test description',
+            'class' => 'PRIVATE',
+            'transp' => 'OPAQUE',
+            'rrule' => 'FREQ=DAILY',
+        ];
+
+        $instance = $this->builder->createEventInstanceWithInput(
+            $event,
+            $input
+        );
+
+        // Check start value and timezone. Same for end
+        $this->assertEquals($start, $instance->getStart());
+        $this->assertEquals($this->timezone, $instance->getStart()->getTimeZone());
+        $this->assertEquals($end, $instance->getEnd());
+        $this->assertEquals($this->timezone, $instance->getEnd()->getTimeZone());
+        $this->assertFalse($instance->isAllDay());
+
+        $this->assertEquals($input['summary'], $instance->getSummary());
+        $this->assertEquals($input['location'], $instance->getLocation());
+        $this->assertEquals($input['description'], $instance->getDescription());
+        $this->assertEquals($input['class'], $instance->getClass());
+        $this->assertEquals($input['transp'], $instance->getTransp());
+        $this->assertEquals($input['rrule'], $instance->getRepeatRule());
+    }
+
+    public function testCreateEventInstanceWithInputAllDay()
+    {
+        $event = $this->builder->createEvent('ABCDEFG');
+
+        // 2015-08-25 all day event
+        $start = new \DateTime('2015-08-25 00:00:00', $this->utc);
+        $end = new \DateTime('2015-08-25 00:00:00', $this->utc);
+
+        $input = [
+            'start' => $start->format(self::INPUT_DATETIME_FORMAT),
+            'end' => $end->format(self::INPUT_DATETIME_FORMAT),
+            'allday' => 'true',
+        ];
+
+        $instance = $this->builder->createEventInstanceWithInput(
+            $event,
+            $input
+        );
+
+        $this->assertTrue($instance->isAllDay());
+        $this->assertEquals($start, $instance->getStart());
+
+        // All day events have DTEND = real end + 1 day
+        $all_day_end = clone $end;
+        $all_day_end->modify('+1 day');
+        $this->assertEquals($all_day_end, $instance->getEnd());
+    }
+
+    public function testCreateEventInstanceWithInputRecurrenceId()
+    {
+        $event = $this->builder->createEvent('ABCDEFG');
+        $instance = $event->createEventInstance();
+        $instance->setStart(new \DateTime('2015-08-25 19:10:00', $this->timezone));
+        $instance->setSummary('Test event');
+        $instance->setRepeatRule('FREQ=DAILY');
+        $event->storeInstance($instance);
+
+        $start = new \DateTime('2015-08-26 18:00:00', $this->utc);
+        $end = new \DateTime('2015-08-26 18:45:00', $this->utc);
+        $input = [
+            'start' => $start->format(self::INPUT_DATETIME_FORMAT),
+            'end' => $end->format(self::INPUT_DATETIME_FORMAT),
+            'summary' => 'My exception',
+            'recurrence_id' => '20150826T174500Z',
+            'rrule' => 'FREQ=DAILY',
+        ];
+
+        $instance = $this->builder->createEventInstanceWithInput(
+            $event,
+            $input
+        );
+
+        $this->assertTrue(
+            $instance->isException(),
+            'Built instance was not marked as exception'
+        );
+        $this->assertEmpty(
+            $instance->getRepeatRule(),
+            'RRULE is set on exceptions when building them from input'
+        );
+    }
+
+    public function testCreateEventInstanceWithInputReminders()
+    {
+        $event = $this->builder->createEvent('ABCDEFG');
+
+        $start = new \DateTime('2015-08-25 19:24:00', $this->utc);
+        $end = new \DateTime('2015-08-25 20:24:00', $this->utc);
+
+        $input = [
+            'start' => $start->format(self::INPUT_DATETIME_FORMAT),
+            'end' => $end->format(self::INPUT_DATETIME_FORMAT),
+            'reminders' => [
+                'count' => [ 0, 7 ],
+                'unit' => [ 'minutes', 'days' ],
+            ],
+        ];
+
+        $instance = $this->builder->createEventInstanceWithInput(
+            $event,
+            $input
+        );
+
+        $reminders = $instance->getReminders();
+
+        $this->assertCount(2, $reminders, 'Total reminders do not match input');
+        $this->assertEquals(
+            [0, 'minutes'],
+            $reminders[0]->getParsedWhen()
+        );
+
+        $this->assertEquals(
+            [1, 'weeks'],
+            $reminders[1]->getParsedWhen()
         );
     }
 }
