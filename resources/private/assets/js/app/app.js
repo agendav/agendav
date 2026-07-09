48ca78d36d4674f9abf1d4c85e94bd9a01e722e5
Alexander Tetzlav <tetzlav@web.de>
2026-07-09 10:30:07 +0200

    Show calendar name in event tooltips

--- resources/private/assets/js/app/app.js
+++ resources/private/assets/js/app/app.js
@@ -1690,11 +1690,14 @@ var event_render_callback = function event_render_callback(event, element) {
     element.find('.fc-title').append(icon_html);
   }
 
-  // Hover tooltip with full time and title
+  // Hover tooltip with calendar name, full time and title
   var fmt = AgenDAVDateAndTime.momentFormat[AgenDAVUserPrefs.time_format];
-  var tooltip = event.allDay
-    ? event.title
-    : event.start.format(fmt) + ' - ' + event.end.format(fmt) + ' ' + event.title;
+  var tooltip = '[' + get_calendar_displayname(event.calendar) + '] ';
+  if (event.start && event.end) {
+    tooltip += event.start.format(fmt) + ' - ' + event.end.format(fmt) + ' ' + event.title;
+  } else {
+    tooltip += event.title;
+  }
   element.attr('title', tooltip);
 
 };
